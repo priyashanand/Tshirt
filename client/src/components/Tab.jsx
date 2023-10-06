41df69beea6d3294763dc73ac781e73d9b5f6f34
priyashanand <anandpriyash17@gmail.com>
2023-10-06 21:24:47 +0530

    Its almost done 1.34.39

---
+++ client/src/components/Tab.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useSnapshot } from 'valtio'
+import state from '../store'
+
+
+
+const Tab = ({tab, isFilterTab, isActiveTab, handleClick}) => {
+  const snap = useSnapshot(state)
+
+  const activeStyles = isFilterTab && isActiveTab 
+  ? { backgrondColor: snap.color, opacity: 0.5 }
+  : { backgrondColor: 'transparent', opacity: 1 }
+
+  return (
+    <div
+    key={tab.name}
+    className={`tab-btn ${isFilterTab ? 'rounded-full galssmorphism' : 'rounded-4'}`}
+    onClick={handleClick}
+    style={activeStyles}
+    >
+      <img 
+      src={tab.icon}
+      alt={tab.name}
+      className={`${isFilterTab ? 'w-2/3 h-2/3' : 'w-11/12 h-11/12 object-contain'}`}
+      />
+    </div>
+  )
+}
+
+export default Tab
